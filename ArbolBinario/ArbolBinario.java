f83d07401e2fba1175503ccb89da970260ae94fe
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-05-02 16:00:04 +0000

    cambios arbolbinario

---
+++ ArbolBinario/ArbolBinario.java
@@ -0,0 +1,41 @@
+package ArbolBinario;
+
+/**
+ * ArbolBinario
+ */
+public class ArbolBinario {
+    //raiz del arbol
+    private nodo raiz;
+    //constructor
+    public ArbolBinario(){
+        raiz = null;
+    }
+    public ArbolBinario(Nodo raiz){
+        this.raiz = raiz;
+    }
+    public void setRaiz(nodo raiz){
+        this.raiz = raiz;
+    }
+    private void PreOrden(nodo aux){
+        if(aux != null){
+           visitar(aux);
+           PreOrden(aux.getIzquierdo());
+              PreOrden(aux.getDerecho());
+        }
+    }
+    private void InOrden(nodo aux){
+        if(aux != null){
+           InOrden(aux.getIzquierdo());
+           visitar(aux);
+           InOrden(aux.getDerecho());
+        }
+    }      
+    private void PostOrden(nodo aux){
+        if(aux != null){
+           PostOrden(aux.getIzquierdo());
+           PostOrden(aux.getDerecho());
+           visitar(aux);
+        }
+    } 
+     
+}
